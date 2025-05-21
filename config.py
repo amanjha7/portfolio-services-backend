72467b5e7e1d7afa308ea3f37bfecd011f400fcc
Aman Jha <amanj@pronnel.com>
2025-05-21 13:58:35 +0530

    add mail server stuffs

--- config.py
+++ config.py
@@ -4,3 +4,11 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI", "sqlite:///app.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY", "super-secret-key")
+
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'amanjhavdjs12tha@gmail.com'  # ✅ Correct this
+    MAIL_PASSWORD = 'jsig wppi nlsg awrv'         # ✅ Your app password
+    MAIL_DEFAULT_SENDER = 'amanjhavdjs12tha@gmail.com'
+
